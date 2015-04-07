9b32cc2e81a5f2530ac493e4a75ea9775799d9f3
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-04-07 11:44:21 -0400

    added image files to source folder

--- src/PuzzlePanel.java
+++ src/PuzzlePanel.java
@@ -1,7 +1,50 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class PuzzlePanel {
+import javax.swing.JPanel;
+
+public class PuzzlePanel extends JPanel implements MouseListener {
+	Puzzle pu;
+
+	public void solve() {
+		pu.solve();
+	}
+
+	public void reset() {
+
+	}
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
