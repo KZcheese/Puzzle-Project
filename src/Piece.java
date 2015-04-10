70cc69bb85da8031b0b0e8924f6a0c29b4c0f6ff
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-04-10 11:43:20 -0400

    Lots of stuff

--- src/Piece.java
+++ src/Piece.java
@@ -1,3 +1,21 @@
 public class Piece {
 
+	public static final int HEARTS_OUT = 0;
+	public static final int HEARTS_IN = 0;
+	public static final int CLUBS_OUT = 0;
+	public static final int CLUBS_IN = 0;
+	public static final int DIAMONDS_IN = 0;
+	public static final int DIAMONDS_OUT = 0;
+	public static final int SPADES_IN = 0;
+	public static final int SPADES_OUT = 0;
+
+	public Piece(int north, int east, int south, int west) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void rotate(int numquadrants) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/PuzzlePanel.java
+++ src/PuzzlePanel.java
@@ -1,18 +1,42 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class PuzzlePanel extends JPanel implements MouseListener,
 		MouseWheelListener {
-	Puzzle pu;
-	Piece[] pieces;
-	PieceComponent[] pieceComponents;
+	private static final int H_OUT = Piece.HEARTS_OUT, H_IN = Piece.HEARTS_IN,
+			C_OUT = Piece.CLUBS_OUT, C_IN = Piece.CLUBS_IN,
+			D_IN = Piece.DIAMONDS_IN, D_OUT = Piece.DIAMONDS_OUT,
+			S_IN = Piece.SPADES_IN, S_OUT = Piece.SPADES_OUT;
+	private Puzzle pu;
+	private Piece[] pieces = { new Piece(C_OUT, H_OUT, D_IN, S_IN),
+			new Piece(S_OUT, D_OUT, S_IN, H_IN),
+			new Piece(H_OUT, S_OUT, S_IN, C_OUT),
+			new Piece(H_OUT, D_OUT, S_IN, S_IN),
+			new Piece(S_OUT, S_OUT, H_IN, S_IN),
+			new Piece(H_OUT, D_OUT, D_IN, H_IN),
+			new Piece(S_OUT, D_OUT, H_IN, D_IN),
+			new Piece(C_OUT, H_OUT, S_IN, H_IN),
+			new Piece(C_OUT, C_IN, D_IN, D_OUT) };
+	private PieceComponent[] pieceComponents = new PieceComponent[pieces.length];
+
+	public PuzzlePanel() throws IOException {
+		for (int i = 0; i < pieces.length; i++) {
+			pieceComponents[i] = new PieceComponent(pieces[i],
+					ImageIO.read(new File("/img/piece_" + (i + 1) + ".png")));
+
+		}
+	}
 
 	public void solve() {
 		pu.solve();
@@ -20,6 +44,10 @@ public class PuzzlePanel extends JPanel implements MouseListener,
 
 	public void reset() {
 		pu.restart();
+	}
+
+	public static void main(String[] args) throws IOException {
+		Image test = ImageIO.read(new File("/img/piece_1.png"));
 	}
 
 	public void paintComponent(Graphics g) {
